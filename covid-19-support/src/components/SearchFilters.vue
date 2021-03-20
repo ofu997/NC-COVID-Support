699701ddd3ea66ee8fa47c74f090896faf206a47
oliver fu <ofu997@gmail.com>
2021-03-19 21:19:18 -0700

    add fetching method

--- covid-19-support/src/components/SearchFilters.vue
+++ covid-19-support/src/components/SearchFilters.vue
@@ -8,31 +8,42 @@
 </template>
 
 <script>
-import { needCategories } from '../constants'
+// import { needCategories } from '../constants'
 
 export default {
   data() {
     return {
       locationData: location,
-      showListing: this.showList
+      showListing: this.showList,
+      needCategories: null
     }
+  },
+  mounted() {
+    fetch('https://nccovid.vizioz.com/api/v1/resource/settings', {
+      mode: 'cors'
+    })
+      .then((response) => response.json())
+      .then((data) => (this.needCategories = data))
+      .catch((error) => console.error(error))
   },
   props: {
     need: String
   },
   computed: {
     needOptionGroups() {
-      const categories = needCategories.categories
+      const categories = this.needCategories.categories
       const needOptions = [{ value: 'none', text: this.$tc('label.selectacategory', 1) }]
       categories.forEach((category) => {
         if (category.subcategories != undefined) {
           const label = category.name
           const myOptions = []
           category.subcategories.forEach((subcategory) => {
-            const text = 'category.' + subcategory.code
+            // const text = 'category.' + subcategory.code
+            const text = 'category.' + subcategory.name
             myOptions.push({
               text: this.$t(text),
-              value: subcategory.code
+              // value: subcategory.code
+              value: subcategory.name
             })
           })
           needOptions.push({
@@ -40,10 +51,12 @@ export default {
             options: myOptions
           })
         } else {
-          const text = 'category.' + category.code
+          // const text = 'category.' + category.code
+          const text = 'category.' + category.name
           needOptions.push({
             text: this.$t(text),
-            value: category.code
+            // value: category.code
+            value: category.name
           })
         }
       })
@@ -52,12 +65,12 @@ export default {
     needOptions() {
       return [
         { value: 'selectacategory', text: this.$tc('label.selectacategory', 1) },
-        { value: 'meal', text: this.$tc('category.meal', 2) },
-        { value: 'family', text: this.$tc('category.family', 2) },
-        { value: 'grocery', text: this.$tc('category.grocery', 2) },
-        { value: 'pharmacy', text: this.$tc('category.pharmacy', 1) },
+        { value: 'meal', text: this.$tc('category.Restaurants', 2) },
+        { value: 'family', text: this.$tc('category.Prepared Family Meals', 2) },
+        { value: 'grocery', text: this.$tc('category.Groceries', 2) },
+        { value: 'pharmacy', text: this.$tc('category.Pharmacies', 1) },
         { value: 'food_bev', text: this.$tc('category.food_bev', 2) },
-        { value: 'pet', text: this.$t('category.pet') }
+        { value: 'pet', text: this.$t('category.Pet Supplies') }
       ]
     }
   }
